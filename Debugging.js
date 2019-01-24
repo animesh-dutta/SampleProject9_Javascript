7aa84e3c987dc055434d9acb57a944fbbd74df08
Animesh Dutta <34543420+animesh-dutta@users.noreply.github.com>
2019-01-24 13:43:32 +0530

    Update Debugging.js

--- Debugging.js
+++ Debugging.js
@@ -4,9 +4,10 @@ Created On:Dec 30,2017
 File Description:External Javascript file for Debugging
 File Name:Debugging.js
 */
+
 function Debugging(){
-var x="How are you doing !";
-alert(x);
-console.log(x);
-document.write(x);
+  var x="How are you doing !";
+  alert(x);
+  console.log(x);
+  document.write(x);
 }
